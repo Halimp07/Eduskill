c8a4a69bcd3803d9dfc8b7b2d3547f562026e1f9
Halimp <halimputra0701@gmail.com>
2023-08-28 19:55:05 +0700

    feat: add aos animation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import hero from "../../assets/hero_eduskill.png";
 import { bg_eduskill_1 } from "../../Data";
 import {
@@ -11,8 +11,14 @@ import {
 } from "../../Data";
 import { motion } from "framer-motion";
 import { Services, Review, Classes, Teacher } from "../../index"; // Adjust the import paths accordingly
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   const container = {
     hidden: {
       opacity: 0,
@@ -39,6 +45,7 @@ const Home = () => {
   return (
     <div className="md:section container mx-auto pt-12 pb-24 px-6 " id="home">
       <img
+        data-aos="zoom-in"
         src={bg_eduskill_1}
         alt=""
         className="absolute bg-no-repeat z-0 bottom-0 w-[20rem] top-14 left-0 right-0 lg:w-[25rem] lg:top-20 lg:left-24 lg:right-0 md:w-[25rem] md:top-20 md:left-24 md:right-0"
@@ -46,25 +53,42 @@ const Home = () => {
       <div className="md:flex items-center justify-center relative">
         {/* First Column */}
         <div className="md:w-1/2 mt-28 md:mb-32 relative z-10">
-          <div className="sm:text-[2.5rem] text-[1.825rem] font-bold">
+          <div
+            data-aos="fade-right"
+            data-aos-delay="300"
+            className="sm:text-[2.5rem] text-[1.825rem] font-bold"
+          >
             Upgrade <span className="text-Orange_Primary">Skillmu</span> <br />{" "}
             Bersama <span className="text-Orange_Primary">EDUSKILL</span>
           </div>
-          <p className="text-sm leading-7 text-gray max-w-md">
+          <p
+            data-aos="fade-right"
+            data-aos-delay="500"
+            className="text-sm leading-7 text-gray max-w-md"
+          >
             Eduskill Menyediakan Kelas Design, Web Development, Business Plan
             Yang Cocok Untuk Mahasiswa Maupun Pemula
           </p>
           <div className="mt-6">
-            <button className="px-6 py-3 font-bold text-white bg-Orange_Primary rounded-lg text-sm">
+            <button
+              data-aos="zoom-in"
+              data-aos-delay="600"
+              className="px-6 py-3 font-bold text-white bg-Orange_Primary rounded-lg text-sm"
+            >
               Daftar
             </button>
-            <button className="px-6 py-3 font-bold text-Orange_Teritiary bg-[#F4EBFF] rounded-lg text-sm ml-4">
+            <button
+              data-aos="zoom-in"
+              data-aos-delay="700"
+              className="px-6 py-3 font-bold text-Orange_Teritiary bg-[#F4EBFF] rounded-lg text-sm ml-4"
+            >
               CS Admin
             </button>
             <div className="mt-10 relative z-10">
               <div className="grid grid-cols-3 md:grid md:grid-cols-3 gap-2 md:gap-4">
                 {focus.map((item) => (
                   <div
+                    data-aos="flip-down"
                     className="md:flex md:items-center md:justify-start gap-2 md:gap-2"
                     key={item.id}
                   >
@@ -89,7 +113,11 @@ const Home = () => {
 
         <div className="md:flex items-center justify-center relative">
           {/* First Div */}
-          <div className="relative  ">
+          <div
+            data-aos="zoom-in-up"
+            data-aos-delay="200"
+            className="relative  "
+          >
             <img
               src={hero}
               alt=""
@@ -98,21 +126,29 @@ const Home = () => {
           </div>
           <div className="md:w-full absolute">
             <img
+              data-aos="zoom-in"
+              data-aos-delay="600"
               src={bg_eduskill_2}
               alt=""
               className="md:w-4/6 absolute top-4 -right-16 z-10"
             />
             <img
+              data-aos="zoom-in-left"
+              data-aos-delay="600"
               src={layers_1}
               alt=""
               className="md:w-5/12 absolute -bottom-0 -right-10 z-10"
             />
             <img
+              data-aos="zoom-in-right"
+              data-aos-delay="600"
               src={layers_2}
               alt=""
               className="md:w-6/12 absolute -bottom-14 -left-20 z-10"
             />
             <img
+              data-aos="zoom-in-left"
+              data-aos-delay="500"
               src={layers_3}
               alt=""
               className="md:w-5/12 absolute top-32 -right-0 z-10"
@@ -122,24 +158,27 @@ const Home = () => {
       </div>
 
       <div class="flex flex-col pt-6 md:flex-row items-center justify-center md:justify-start">
-        <div class="md:mt-0 mr-0 md:mr-16 text-center md:text-left mb-4 md:mb-0">
+        <div
+          data-aos="fade-down"
+          class="md:mt-0 mr-0 md:mr-16 text-center md:text-left mb-4 md:mb-0"
+        >
           <p class="font-bold text-2xl md:text-xl text-Orange_Primary">10+</p>
           <p class="text-2xl md:text-xl">Collaborations</p>
         </div>
-        <motion.div
-          variants={container}
-          initial="hidden"
-          whileInView="visible"
+        <div
+       
+         
           className="flex items-center justify-center md:justify-center flex-wrap gap-16 pt-4 md:pt-0"
         >
           {logos.map((logo, index) => (
-            <motion.div variants={item} className="w-9" key={index}>
+            <div   data-aos="flip-left"
+            data-aos-delay="200"  className="w-9" key={index}>
               <img src={logo} alt="" className="w-full object-cover" />
-            </motion.div>
+            </div>
           ))}
-        </motion.div>
+        </div>
       </div>
-      <Services/>
+      <Services />
       <Review />
       <Classes />
       <Teacher />
