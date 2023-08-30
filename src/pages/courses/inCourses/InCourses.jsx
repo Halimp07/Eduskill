f0c247ea25d54539970cc724106062a1a6abb6e3
Halimp <halimputra0701@gmail.com>
2023-08-30 10:24:14 +0700

    minor refactor: remove AI

--- src/pages/courses/inCourses/InCourses.jsx
+++ src/pages/courses/inCourses/InCourses.jsx
@@ -4,7 +4,6 @@ import { classEdu } from "../../../Data";
 import VideoComponent from "./VideoComponent";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
-import { AiEduskill } from "../../../index"; // Adjust the import paths accordingly
 
 const InCourses = () => {
   const { namelink } = useParams();
@@ -63,7 +62,6 @@ const InCourses = () => {
       </div>
       <div className="mt-10">
 
-      <AiEduskill />
       </div>
     </div>
   );
