152f7d1d6b451658802a020a5308c27e7c7d57b6
Halimp <halimputra0701@gmail.com>
2023-09-11 01:06:05 +0700

    major refactor: change dependencies

--- src/pages/courses/inCourses/Test/Compiler.jsx
+++ src/pages/courses/inCourses/Test/Compiler.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ReactHtmlParser from "react-html-parser";
+import parse from "html-react-parser";
 import { AiOutlineExpandAlt } from "react-icons/ai";
 
 const Compiler = () => {
@@ -30,23 +30,23 @@ const Compiler = () => {
   return (
     <div className="flex">
       {/* Left Section */}
-        <div className="mb-0 mr-4">
-          <div className="flex justify-between bg-Orange_Primary rounded-t-xl p-2">
-            <h2 className="text-base font-light ml-4 text-white">HTML</h2>
-            <h2 className="text-base font-light ml-4 text-white">Salin Code</h2>
-          </div>
-          <textarea
-            placeholder="<!DOCTYPE html>
+      <div className="mb-0 mr-4">
+        <div className="flex justify-between bg-Orange_Primary rounded-t-xl p-2">
+          <h2 className="text-base font-light ml-4 text-white">HTML</h2>
+          <h2 className="text-base font-light ml-4 text-white">Salin Code</h2>
+        </div>
+        <textarea
+          placeholder="<!DOCTYPE html>
 <html>
   <body>
     <h1>Hello World!</h1>
   </body>
 </html>"
-            rows="6"
-            className="w-[390px] h-[408px] border border-[#E7E7E7] bg-[#E7E7E7] rounded-b-xl placeholder:p-4" // Add border-gray-300 class for gray border
-            value={htmlCode}
-            onChange={(e) => setHtmlCode(e.target.value)}
-          />
+          rows="6"
+          className="w-[390px] h-[408px] border border-[#E7E7E7] bg-[#E7E7E7] rounded-b-xl placeholder:p-4" // Add border-gray-300 class for gray border
+          value={htmlCode}
+          onChange={(e) => setHtmlCode(e.target.value)}
+        />
       </div>
 
       {/* Right Section */}
@@ -60,7 +60,7 @@ const Compiler = () => {
               </h2>
             </div>
             <div className="w-[310px] h-[180px] border border-white bg-white rounded-b-xl p-4">
-              {ReactHtmlParser(output)}
+              {parse(output)} {/* Use parse to render the HTML */}
             </div>
           </div>
 
@@ -79,7 +79,7 @@ const Compiler = () => {
             {/* Preview Section */}
             {showPreview && (
               <div className="w-[310px] h-[180px] border-[#667085] bg-[#667085] rounded-b-xl rounded p-4 text-white">
-                <div dangerouslySetInnerHTML={{ __html: output }} />
+                {parse(output)} {/* Use parse to render the HTML */}
               </div>
             )}
           </div>
